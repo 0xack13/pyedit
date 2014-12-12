af91b26dc89de9f7421648a55c410a3fc22ff37b
Saleh Albatati <0xack13@gmail.com>
2014-12-12 18:30:20 +0300

    Add text file

--- pyedit.py
+++ pyedit.py
@@ -1,6 +1,6 @@
 # /Users/Saleh/src/ios/SwiftReminder/Salawat.txt
 # print open('/Users/Saleh/src/ios/SwiftReminder/Salawat.txt').read().decode('string-escape').decode("utf-8")
-#
+# sed -i 's/;/ /g'
 with open("/Users/Saleh/src/ios/SwiftReminder/Salawat.txt") as f:
     content = f.readlines()	
 y = [unicode(i, "utf8") for i in content]
