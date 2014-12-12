e72f2a2e988afb3336bb5eeddc841adb1554f509
Saleh Albatati <0xack13@gmail.com>
2014-12-12 18:22:46 +0300

    Print unicode format

--- pyedit.py
+++ pyedit.py
@@ -1,3 +1,7 @@
+# /Users/Saleh/src/ios/SwiftReminder/Salawat.txt
+# print open('/Users/Saleh/src/ios/SwiftReminder/Salawat.txt').read().decode('string-escape').decode("utf-8")
+#
 with open("/Users/Saleh/src/ios/SwiftReminder/Salawat.txt") as f:
-    content = f.readlines()
-    print(content)
+    content = f.readlines()	
+y = [unicode(i, "utf8") for i in content]
+print y
