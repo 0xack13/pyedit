85164c21626240d63efcbb3bf38a119e830bc5a4
Saleh Albatati <0xack13@gmail.com>
2014-12-15 01:50:09 +0300

    Count # of lines

--- pyedit.py
+++ pyedit.py
@@ -1,7 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # /Users/Saleh/src/ios/SwiftReminder/Salawat.txt
 # print open('/Users/Saleh/src/ios/SwiftReminder/Salawat.txt').read().decode('string-escape').decode("utf-8")
 # sed -i 's/;/ /g'
+
+num_lines = sum(1 for line in open("/Users/Saleh/src/ios/SwiftReminder/Salawat.txt"))
+
+print num_lines
+
 with open("/Users/Saleh/src/ios/SwiftReminder/Salawat.txt") as f:
     content = f.readlines()	
 y = [unicode(i, "utf8") for i in content]
 print y
+
