095d78eec036c204a1ad0decbbaaa0003c702891
Saleh Albatati <0xack13@gmail.com>
2014-12-15 14:38:16 +0300

    Count empty and non-empty lines in a file

--- pyedit.py
+++ pyedit.py
@@ -4,12 +4,15 @@
 # print open('/Users/Saleh/src/ios/SwiftReminder/Salawat.txt').read().decode('string-escape').decode("utf-8")
 # sed -i 's/;/ /g'
 
-num_lines = sum(1 for line in open("/Users/Saleh/src/ios/SwiftReminder/Salawat.txt"))
+num_lines = sum(1 for line in open("/Users/Saleh/src/0xack13/pyedit/Salawat.txt"))
 
-print num_lines
+# if line.rstrip())
+num_lines_empty = sum(1 for line in open("/Users/Saleh/src/0xack13/pyedit/Salawat.txt") if line.rstrip())
+
+print num_lines, " Empty: ", num_lines_empty
 
 with open("/Users/Saleh/src/ios/SwiftReminder/Salawat.txt") as f:
     content = f.readlines()	
 y = [unicode(i, "utf8") for i in content]
-print y
+#print y
 
